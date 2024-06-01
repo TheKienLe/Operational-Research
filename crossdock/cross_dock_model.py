9bea0b6c2ec2c04587f7eb4587f352ea112ea174
TheKienLe <89660234+TheKienLe@users.noreply.github.com>
2024-06-01 13:52:54 +0700

    Done_crossdock

--- crossdock/cross_dock_model.py
+++ crossdock/cross_dock_model.py
@@ -24,31 +24,40 @@ def cross_dock_model(name):
     # floor capacity
     cap = df_to_list(pd.read_excel(
         name, "location_floor_cluster"), "floor", "cap")
+    print("floor cap:", cap)
 
     # location each floor in each cluster
     y = df_to_list(pd.read_excel(name, "location_floor_cluster"),
                    "cluster", "floor", "allocation_in_cluster")
+    print("cluster location", y)
 
     # distance from floor to shipping point
     distS = df_to_list(pd.read_excel(
         name, "distance_floor_shipping"), "floor", "distance")
+    print("floor distance", distS)
 
     # distance picking carton in cluster
     distPcb = df_to_list(pd.read_excel(name, "distance_cluster").query(
         "type == 'carton'"), "cluster", "distance")
+    print("Distance carton", distPcb)
 
     # distance picking plastic in cluster
     distPpb = df_to_list(pd.read_excel(name, "distance_cluster").query(
         "type == 'plastic'"), "cluster", "distance")
-
+    print("Distance plastic", distPpb)
+    
     # number of pallets shipped to store s within 24 hrs
     PS = df_to_list(pd.read_excel(name, "demand"), "store", "shipped")
+    print("Num pallet shipped", PS)
 
     # requirement number of pallets store s for within 24 hrs
-    D = df_to_list(pd.read_excel(name, "demand"), "store", "requirement")
+    D = df_to_list(pd.read_excel(name, "demand"), "store", "demand")
+    print("Store Demand", D)
 
     # space requirement of store s for 24 hrs
     R = df_to_list(pd.read_excel(name, "demand"), "store", "floor_unit")
+    print("R:", R)
+    print("space requirement", R)
 
     # percentage plastic boxes picked compared to total box in 24 hrs
     ppb = 0.5
@@ -78,27 +87,29 @@ def cross_dock_model(name):
     # number of visit to cluster
     NV = dict()
     for cl in CL:
-        NV["cl" + str(cl)] = solver.IntVar(0, solver.infinity(), "")
+        NV["cl" + str(cl)] = solver.NumVar(0, solver.infinity(), "")
     # constraint
     # 3
 
     sumry = summary_df(y)
-    print(sumry)
     for s in S:
         for cl in CL:
             for f in sumry["cl" + str(cl)]:
-                if y[("cl" + str(cl), f)] == 1:
-                    solver.Add(NV["cl" + str(cl)] >= NS["s"+str(s)] *
-                               x[("s" + str(s), f)], "ct3")
+                if y[("cl" + str(cl), f)] >= 1:
+                    solver.Add(NV["cl" + str(cl)] >=
+                               NS["s"+str(s)] * x[("s" + str(s), f)]
+                               , "ct3")
 
     # 4
     for f in F:
-        solver.Add(solver.Sum(x[("s" + str(s), "f" + str(f))] for s in S) <= 1)
+        solver.Add(solver.Sum([x[("s" + str(s), "f" + str(f))] for s in S]) <= 1)
 
     # 5
-    for f in F:
-        solver.Add(solver.Sum(x[("s" + str(s), "f" + str(f))]
-                   for f in F) <= R["s" + str(s)])
+    for s in S:
+        solver.Add(solver.Sum([x[("s" + str(s), "f" + str(f))]
+                   for f in F]) <=
+                   R["s" + str(s)]
+                   , "ct5")
 
     # 6
     for s in S:
@@ -111,27 +122,45 @@ def cross_dock_model(name):
 
     # 7
     for s in S:
-        solver.Add(solver.Sum(x[("s" + str(s), "f" + str(f))] *
-                              cap["f" + str(f)] for f in F) >= D["s" + str(s)])
+        solver.Add(solver.Sum([x[("s" + str(s), "f" + str(f))] *
+                              cap["f" + str(f)] for f in F]) >=
+                              D["s" + str(s)]
+                              , "ct7")
 
     # 8
-    for f in F:
-        for s in S:
-            if R["s" + str(s)] >= 2:
-                solver.Add(x[("s" + str(s), "f" + str(f))] <=
-                           x[("s" + str(s), "f" + str(f+1))] + x[("s" + str(s), "f" + str(f-1))])
+    # for f in F:
+    #     for s in S:
+    #         if R["s" + str(s)] >= 2:
+    #             solver.Add(x[("s" + str(s), "f" + str(f))] <=
+    #                        x[("s" + str(s), "f" + str(f+1))] + x[("s" + str(s), "f" + str(f-1))])
 
     # objective func
     objective = solver.Sum(
-        distPcb["cl" + str(cl)] * NV["cl" + str(cl)] * (1-ppb) + distPpb["cl" + str(cl)] * NV["cl" + str(cl)] * ppb for cl in CL) + solver.Sum(distS["f" + str(f)] * x[("s" + str(s), "f" + str(f))] * PS["s" + str(s)] for f in F for s in S)
+        distPcb["cl" + str(cl)] * NV["cl" + str(cl)] * (1-ppb) + distPpb["cl" + str(cl)] * NV["cl" + str(cl)] * ppb for cl in CL)\
+    + solver.Sum(distS["f" + str(f)] * x[("s" + str(s), "f" + str(f))] * PS["s" + str(s)] for f in F for s in S)
     solver.Minimize(objective)
+
+    # Sets a time limit of 1 hour.
+    solver.SetTimeLimit(10*60*1000)
+
+    # set a minimum gap limit for the integer solution during branch and cut
+    gap = 0.15
+    solverParams = pywraplp.MPSolverParameters()
+    solverParams.SetDoubleParam(solverParams.RELATIVE_MIP_GAP, gap)
 
     # solve
     print(f"Solving with {solver.SolverVersion()}")
     status = solver.Solve()
 
     if status == pywraplp.Solver.OPTIMAL or status == pywraplp.Solver.FEASIBLE:
-        print(f"Minimum distance {solver.Objective().value()}")
+        print(f"Minimum distance {solver.Objective().Value()}")
+        for cl in CL: 
+            print(f"NV{cl}:", NV["cl" + str(cl)].solution_value())
+        for s in S:
+            for f in F:
+                if x[("s" + str(s), "f" + str(f))].solution_value() == 1:
+                    print(f"x{s, f}:", 1)
+
     else:
         print("No solution")
 
